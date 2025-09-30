540d198f6234dee656d62ab912808f5a3c7176b4
ruizcristians <cristianalbertoruizsuarez@gmail.com>
2025-09-30 11:25:56 -0500

    todo arreglado

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,13 +9,16 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
-
     <title>@yield('title')</title>
+
+    <style>
+        body {
+            padding-bottom: 70px; /* espacio para que el footer no tape el contenido */
+        }
+    </style>
 </head>
 
 <body>
-
-
     @yield('titleContent')
 
     <div class="container">
@@ -24,10 +27,9 @@
         </div>
     </div>
 
-
     <footer class="bg-dark text-white text-center py-3 fixed-bottom">
-    Autor: Yerli Blanco
-</footer>
+        Autor: Yerli Blanco
+    </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
