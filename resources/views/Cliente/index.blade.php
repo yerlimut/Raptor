b350abf529db84deb477748b32d07f6f76e13614
lauyer chia <karinachia2005@gmail.com>
2025-10-24 08:14:52 -0500

    cambios

--- resources/views/Cliente/index.blade.php
+++ resources/views/Cliente/index.blade.php
@@ -147,7 +147,7 @@ Gestión de Clientes
     @endif
 
     <div class="container">
-        <table  id="myTable" class="table table-bordered table-hover">
+        <table id="myTable" class="table table-bordered table-hover w-75 mx-auto small align-middle" >
             <thead class="table-dark">
                 <tr>
                     <th>ID</th>
