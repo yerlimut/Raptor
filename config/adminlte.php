a9af7d35eadc487f4189465f8911c405dea404df
lauyer chia <karinachia2005@gmail.com>
2025-11-04 08:38:34 -0500

    cambios

--- config/adminlte.php
+++ config/adminlte.php
@@ -110,7 +110,7 @@ return [
     */
 
     'preloader' => [
-        'enabled' => true,
+        'enabled' => false,
         'mode' => 'fullscreen',
         'img' => [
             'path' => 'imagenes/RaptorLogo.png',
--- resources/views/Mecanico/create.blade.php
+++ resources/views/Mecanico/create.blade.php
@@ -5,14 +5,10 @@ Crear Mecánico
 @endsection
 @section('content_header')
 
-<h1 class="fw-bold display-6 mb-0">RAPTOR </h1>
 @endsection
 
 @section('content')
-<img src="{{ asset('imagenes/RAPTOR.png') }}"
-    alt="RAPTOR"
-    class="position-fixed rounded-4"
-    style="top: 40px; right: 10px; max-height: 130px; z-index: 1000; background-color: transparent;">
+
 
 
 <div class="container mt-5">
