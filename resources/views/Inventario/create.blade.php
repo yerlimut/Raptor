14839472250e5e2e4dbad850261cd8f3454437f1
Yerli Blanco <karinachia2005@gmail.com>
2025-11-04 08:39:31 -0500

    cambios

--- resources/views/Inventario/create.blade.php
+++ resources/views/Inventario/create.blade.php
@@ -6,15 +6,9 @@ Crear Inventario
 
 @section('content_header')
 
-<h1 class="fw-bold display-6 mb-0">RAPTOR </h1>
 @endsection
 
 @section('content')
-<img src="{{ asset('imagenes/RAPTOR.png') }}"
-    alt="RAPTOR"
-    class="position-fixed rounded-4"
-    style="top: 40px; right: 10px; max-height: 130px; z-index: 1000; background-color: transparent;">
-
 
 <div class="container mt-5">
     <h1 class="text-center"><i class="bi bi-box-seam"></i> Crear Inventario</h1>
