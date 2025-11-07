72f01a6a4a66090bd4bfbde1b1e7e6af94868ea5
Yerli Blanco <karinachia2005@gmail.com>
2025-11-07 08:01:41 -0500

    Cambios de Preorden

--- app/Http/Controllers/CategoriaRepuestoController.php
+++ app/Http/Controllers/CategoriaRepuestoController.php
@@ -100,7 +100,14 @@ class CategoriaRepuestoController extends Controller
     public function destroy($id)
     {
         $categoriasRepuesto = categoriaRepuesto::findorfail($id);
-        $categoriasRepuesto->delete();
-        return redirect()->route('categoriaRepuesto.index')->with('success', 'Categoria del Repuesto Eliminada correctamente');
+        try{
+            $categoriasRepuesto->delete();
+             return redirect()->route('categoriaRepuesto.index')->with('success', 'Categoria del Repuesto Eliminada correctamente');
+        }catch(\Illuminate\Database\QueryException $e){
+            return redirect()->route('categoriaRepuesto.index')->with('error', 'No se puede eliminar la categoria porque tiene repuestos asociados.');
+        }
+
     }
+
+
 }
--- resources/views/CategoriaRepuesto/index.blade.php
+++ resources/views/CategoriaRepuesto/index.blade.php
@@ -96,6 +96,19 @@
 </script>
 @endif
 
+   @if(session('error'))
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            Swal.fire({
+                icon: 'error',
+                title: '¡Atención!',
+                text: "{{ session('error') }}",
+                confirmButtonText: 'Aceptar',
+            });
+        });
+    </script>
+    @endif
+
 <div class="container">
     <table id="myTable" class="table table-bordered table-hover">
         <thead class="table card-header bg-primary text-white">
