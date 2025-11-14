cfd61355d82e80d993de322a73e0080b62404d18
Yerli Blanco <karinachia2005@gmail.com>
2025-11-14 09:00:48 -0500

    Cambio en create

--- resources/views/Preorden/create.blade.php
+++ resources/views/Preorden/create.blade.php
@@ -113,6 +113,7 @@
 </div>
 @endsection
 
+
 @section('js')
 {{-- Dependencias de Select2 --}}
 <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
